63e0ed9fae3cdc6354c3c08bcce7fc86669872e3
sunbin <859990843@qq.com>
2020-08-30 20:41:47 +0800

    第二次

--- dom.js
+++ dom.js
@@ -1,14 +1,14 @@
 window.dom = {
-  find(selector, scope) {
-    return (scope || document).querySelectorAll(selector);
-  },
   style(node, name, value) {
     if (arguments.length === 3) {
+      // dom.style(div, 'color', 'red')
       node.style[name] = value;
     } else if (arguments.length === 2) {
       if (typeof name === "string") {
+        // dom.style(div, 'color')
         return node.style[name];
       } else if (name instanceof Object) {
+        // dom.style(div, {color: 'red'})
         const object = name;
         for (let key in object) {
           node.style[key] = object[key];
@@ -16,6 +16,11 @@ window.dom = {
       }
     }
   },
+
+  find(selector, scope) {
+    return (scope || document).querySelectorAll(selector);
+  },
+
   each(nodeList, fn) {
     for (let i = 0; i < nodeList.length; i++) {
       fn.call(null, nodeList[i]);
